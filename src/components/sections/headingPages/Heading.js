591b3461182d020f2bd58bd28c9257628fdcffff
Zeal Gabriel Ifeanyi <zealgabriels@gmail.com>
2024-01-25 23:27:44 +0100

    Adding little lemon capstone project to GitHub repo

--- src/components/sections/headingPages/Heading.js
+++ src/components/sections/headingPages/Heading.js
@@ -6,7 +6,8 @@ export default function Heading() {
                 <section className="hero-text">
                     <h1>Little Lemon</h1>
                     <h2>GRA, Port Harcourt</h2>
-                    <p className="subsection">Welcome to Little Lemon Restaurant, where modern culinary excellence meets timeless flavors.</p>
+                    <p className="subsection">Welcome to Little Lemon Restaurant, where modern culinary excellence meets timeless flavors. 
+                    Please come and enjoy our wide selection of authentic Mediterranean cuisine!.</p>
                     <br></br>
                     <Link className="action-button" to="/reservations">Reserve a table</Link>
                 </section>
