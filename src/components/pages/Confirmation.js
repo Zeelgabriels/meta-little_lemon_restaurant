18ec4e23c3cd8562d04e1e692b678d0d1a4a38cd
Zeal Gabriel Ifeanyi <zealgabriels@gmail.com>
2024-01-25 22:58:18 +0100

    Adding little lemon capstone project to GitHub repo

--- src/components/pages/Confirmation.js
+++ src/components/pages/Confirmation.js
@@ -11,7 +11,7 @@ export default function Confirmation() {
       <section className="reserve-header-text">
         <h1>Your Reservation is Confirmed!</h1>
         <h4>A confirmation message has been sent to your email.</h4>
-        <h4>Thanks for dining with us!</h4>
+        <h4>Thank you for dining with us!</h4>
       </section>
 
       <section className="redirect-buttons">
--- src/components/sections/headingPages/About.js
+++ src/components/sections/headingPages/About.js
@@ -3,10 +3,11 @@ export default function About() {
         <article className="about-us">
             <section className="hero-text">
                 <h1>Little Lemon</h1>
-                <h2>Chicago</h2>
-                <p className="about-subtext">Little Lemon opened in 1995 by two Italian brothers, Adrian and Mario. 
-                Despite the city's diversity, the two brothers recognized the lack of Mediterranean cuisine in Chicago, 
-                and were inspired to bring the flavors of their hometown in Italy to the people of Chicago. The two brothers 
+                <h2>GRA, Port Harcourt</h2>
+                <p className="about-subtext">Little Lemon was opened to the public in 1995 by an Italian couple, Adrian and Maria, 
+                who recently migrated to the beautiful city of Port Harcourt, Rivers State, the southern region of Nigeria. 
+                Despite the city's diversity, the couple recognized the lack of Mediterranean cuisine in Port Harcourt, 
+                and were inspired to bring the flavors of their hometown in Italy to the people of Rivers State. The couple 
                 continue to oversee the Little Lemon restaurant, nearly thirty years later.</p>
             </section>
 
